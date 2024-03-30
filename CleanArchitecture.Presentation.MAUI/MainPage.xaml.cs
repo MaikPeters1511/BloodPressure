d7fd68caea205dad0e2dd5cdc3352f7906ceca3b
Maiki <bewerbung@maikpeters.org>
2024-03-30 13:07:06 +0100

    Add new files for CleanArchitecture.Presentation.MAUI
    This commit introduces multiple new files necessary for the CleanArchitecture.Presentation.MAUI project. These include elements such as app icons, app.manifest file, new styles in app.css, and additional settings for platforms like Android, iOS, etc. It also includes the initialization of Maui applications along with other necessary components.

---
+++ CleanArchitecture.Presentation.MAUI/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Presentation.MAUI;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ CleanArchitecture.Presentation.MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CleanArchitecture.Presentation.MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
