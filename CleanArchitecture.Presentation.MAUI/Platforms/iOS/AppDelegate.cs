3a2d90ff9312fe738981efed6335df9fde78c63a
Maiki <bewerbung@maikpeters.org>
2024-04-09 08:50:30 +0200

    Delete Projects

--- CleanArchitecture.Presentation.MAUI/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace CleanArchitecture.Presentation.MAUI;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
