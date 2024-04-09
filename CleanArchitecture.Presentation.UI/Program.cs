245683a651d06bc4dc95dce39f48ee441a469502
Maiki <bewerbung@maikpeters.org>
2024-04-09 09:16:19 +0200

    feat: Install Syncfusion

--- CleanArchitecture.Presentation.UI/Program.cs
+++ CleanArchitecture.Presentation.UI/Program.cs
@@ -2,10 +2,18 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CleanArchitecture.Presentation.UI;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Syncfusion Api Key laden aus appsettings
+var configuration = builder.Configuration;
+var syncFusionLicenseKey = configuration.GetValue<string>("Syncfusion:LicenseKey");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(syncFusionLicenseKey);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
