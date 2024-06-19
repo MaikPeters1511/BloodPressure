c48dfc2d4502cba3da3c89af6de5c92e22ac6fc5
Maiki <bewerbung@maikpeters.org>
2024-06-19 10:40:58 +0200

    Add project files for cross-platform Presentation layer using Maui
    This commit introduces a wide array of project, configuration files, UI templates, application metadata and data handling code that form the foundation of the newly implemented cross-platform Presentation layer. With a special focus on Microsoft's Maui, the application is now optimized to work seamlessly across Android, iOS, Windows and MacOS. Notable additions also include app icons, splash screens, entitlements and raw assets for deployment.

---
+++ CleanArchitecture.Presentation.Maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitecture.Presentation.Maui;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ CleanArchitecture.Presentation.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Presentation.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
